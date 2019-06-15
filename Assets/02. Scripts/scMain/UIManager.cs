4eaad544978420acda87b9f100f6069682e8d9d4
jungdo yun <wjdeh10110@gmail.com>
2019-06-15 13:58:57 +0900

    06 15 client over

--- Assets/02. Scripts/scMain/UIManager.cs
+++ Assets/02. Scripts/scMain/UIManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public void OnClickStartButton()
     {
-        Debug.Log("Start Button Clicked!!");
+        SceneManager.LoadScene("scGame_Stage");
+        SceneManager.LoadScene("scGame_Play", LoadSceneMode.Additive);
     }
 }
